70a94efad8bc5f97fac141ce1a1dda249058dd26
Chris <straight.chris@gmail.com>
2023-03-08 17:32:10 -0800

    chore: Update code to improve flexibility and robustness
    This commit improves the flexibility and robustness of the code by utilizing the viper package to read configuration values from a YAML file. The irsendHandler function has been refactored into a higher-order function that returns a Gin handler function, improving readability and maintainability.
    
    A signal handler has been added to gracefully shutdown the server when an interrupt signal is received, ensuring that the server shuts down correctly. Additionally, error handling has been improved throughout the code by providing informative error messages to the user, making it easier to diagnose and fix issues.
    
    Overall, these changes make the code more robust and flexible, allowing for easier configuration changes and better error handling.

--- main.go
+++ main.go
@@ -1,159 +1,87 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
-
-	"github.com/gin-gonic/gin"
-
-	// "net/http"
+	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
-/*
-	CXA81 IRSend Commands:
-
-	0000000000000401 SOURCE_A4
-	0000000000000403 SOURCE_A3
-	0000000000000404 SOURCE_A1
-	0000000000000405 SOURCE_A2
-	0000000000000408 SOURCE_CYCLE
-	0000000000000410 KEY_VOLUMEUP
-	0000000000000411 KEY_VOLUMEDOWN
-	000000000000040c KEY_SLEEP
-	000000000000040d KEY_MUTE_UNMUTE
-	000000000000040e POWER_ON
-	000000000000040f POWER_OFF
-	0000000000000414 DEST_AB
-	000000000000041c DEST_A
-	000000000000041d DEST_B
-	000000000000041e DEST_B1
-	0000000000000423 DEST_A1
-	0000000000000427 DEST_B2
-	0000000000000432 MUTE
-	0000000000000433 UNMUTE
-	0000000000000434 DISP_ON
-	0000000000000435 DISP_OFF
-	000000000000064c POWER_ONOFF
-	000000000000064e POWERON
-	000000000000064f POWEROFF
-
-*/
-
 func main() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yaml")
 
-	fmt.Println("CXA81-IR-Remote-Server")
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s", err))
+	}
 
 	router := gin.Default()
 
-	// SYSTEM ROUTERS
+	router.GET("/poweronoff", irsendHandler(viper.GetString("remote_name"), viper.GetString("poweronoff_command")))
+	router.GET("/poweron", irsendHandler(viper.GetString("remote_name"), viper.GetString("poweron_command")))
+	router.GET("/poweroff", irsendHandler(viper.GetString("remote_name"), viper.GetString("poweroff_command")))
+	router.GET("/volumeup", irsendHandler(viper.GetString("remote_name"), viper.GetString("volumeup_command")))
+	router.GET("/volumedown", irsendHandler(viper.GetString("remote_name"), viper.GetString("volumedown_command")))
+	router.GET("/sleep", irsendHandler(viper.GetString("remote_name"), viper.GetString("sleep_command")))
+	router.GET("/mute", irsendHandler(viper.GetString("remote_name"), viper.GetString("mute_command")))
+	router.GET("/sourceA1", irsendHandler(viper.GetString("remote_name"), viper.GetString("sourceA1_command")))
+	router.GET("/sourceA2", irsendHandler(viper.GetString("remote_name"), viper.GetString("sourceA2_command")))
+	router.GET("/sourceA3", irsendHandler(viper.GetString("remote_name"), viper.GetString("sourceA3_command")))
+	router.GET("/sourceA4", irsendHandler(viper.GetString("remote_name"), viper.GetString("sourceA4_command")))
+	router.GET("/sourcecycle", irsendHandler(viper.GetString("remote_name"), viper.GetString("sourcecycle_command")))
 
-	// /poweronoff POWER_ONOFF Turns the power on OR off.
-	router.GET("/poweronoff", func(c *gin.Context) {
-		cmd := "irsend"
-		args := []string{"SEND_ONCE", "cambridge", "POWER_ONOFF"}
-		if err := exec.Command(cmd, args...).Run(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	})
-
-	// /poweron POWERON Turns the power on.
-	router.GET("/poweron", func(c *gin.Context) {
-		cmd := "irsend"
-		args := []string{"SEND_ONCE", "cambridge", "POWERON"}
-		if err := exec.Command(cmd, args...).Run(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	})
-
-	// /poweroff POWEROFF Turns the power on.
-	router.GET("/poweroff", func(c *gin.Context) {
-		cmd := "irsend"
-		args := []string{"SEND_ONCE", "cambridge", "POWEROFF"}
-		if err := exec.Command(cmd, args...).Run(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	})
-
-	// /volumeup KEY_VOLUMEUP Turns the volume up + 1.
-	router.GET("/volumeup", func(c *gin.Context) {
-		cmd := "irsend"
-		args := []string{"SEND_ONCE", "cambridge", "KEY_VOLUMEUP"}
-		if err := exec.Command(cmd, args...).Run(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	})
-	// /volumedown KEY_VOLUMEDOWN  Turns the volume down - 1.
-	router.GET("/volumedown", func(c *gin.Context) {
-		cmd := "irsend"
-		args := []string{"SEND_ONCE", "cambridge", "KEY_VOLUMEDOWN"}
-		if err := exec.Command(cmd, args...).Run(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	})
-
-	// SOURCE ROUTERS
-
-	router.GET("/sourceA1", func(c *gin.Context) {
-		cmd := "irsend"
-		args := []string{"SEND_ONCE", "cambridge", "SOURCE_A1"}
-		if err := exec.Command(cmd, args...).Run(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	})
-
-	router.GET("/sourceA2", func(c *gin.Context) {
-		cmd := "irsend"
-		args := []string{"SEND_ONCE", "cambridge", "SOURCE_A2"}
-		if err := exec.Command(cmd, args...).Run(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	})
-
-	router.GET("/sourceA3", func(c *gin.Context) {
-		cmd := "irsend"
-		args := []string{"SEND_ONCE", "cambridge", "SOURCE_A3"}
-		if err := exec.Command(cmd, args...).Run(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	})
-
-	router.GET("/sourceA4", func(c *gin.Context) {
-		cmd := "irsend"
-		args := []string{"SEND_ONCE", "cambridge", "SOURCE_A4"}
-		if err := exec.Command(cmd, args...).Run(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	})
-
-	router.GET("/sourcecycle", func(c *gin.Context) {
-		cmd := "irsend"
-		args := []string{"SEND_ONCE", "cambridge", "SOURCE_CYCLE"}
-		if err := exec.Command(cmd, args...).Run(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	})
-
-	localIP, err := GetOutboundIP()
+	localIP, err := getOutboundIP()
 	if err != nil {
-		os.Exit(1)
+		panic(fmt.Errorf("Fatal error getting IP address: %s", err))
 	}
 
-	localPort := "8081"
+	localPort := viper.GetString("port")
 
-	router.Run(localIP + ":" + localPort)
+	server := &http.Server{
+		Addr:    localIP + ":" + localPort,
+		Handler: router,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			panic(fmt.Errorf("Fatal error starting server: %s", err))
+		}
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
+
+	if err := server.Shutdown(context.Background()); err != nil {
+		fmt.Println("Error during server shutdown:", err)
+	}
 }
 
-// Get preferred outbound ip of this machine
-func GetOutboundIP() (string, error) {
+func irsendHandler(remoteName, command string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cmd := exec.Command("irsend", "SEND_ONCE", remoteName, command)
+		if err := cmd.Run(); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send IR command"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "IR command sent successfully"})
+	}
+}
 
+func getOutboundIP() (string, error) {
 	connection, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
 		return "", err
 	}
 	defer connection.Close()
-
 	localAddr := connection.LocalAddr().(*net.UDPAddr)
 
 	return localAddr.IP.String(), nil
